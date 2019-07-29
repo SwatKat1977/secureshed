d2440fd6225eeda4017e64d5712e6ae0440bdd96
cyberlynx <cyberlynx@localhost>
2019-07-29 18:31:23 +0100

    work on keypad

--- src/keypadController/KeypadPanel.py
+++ src/keypadController/KeypadPanel.py
@@ -30,13 +30,17 @@ class KeypadPanel(wx.Panel):
     SequenceTimeout = 5
 
 
-	## KeypadPanel class constructor.
-	#  @param self The object pointer.
-	#  @param parent Parent of the wxPython panel.
+    ## KeypadPanel class constructor.
+    #  @param self The object pointer.
+    #  @param parent Parent of the wxPython panel.
     def __init__(self, parent):
         super().__init__(parent)
 
         self.__authorisationKey = 'authKey'
+
+        self.__keypadDisableTimer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.__keypadDisabledTimedOut,
+            self.__keypadDisableTimer)
 
         self.__additionalHeaders = {
             'authorisationKey' : self.__authorisationKey
@@ -53,14 +57,19 @@ class KeypadPanel(wx.Panel):
  
         self.__CreateUI()
 
+        self.DisableKeypad(5)
 
-	## Create the keypad user interface.
-	#  @param self The object pointer.
+
+    ## Create the keypad user interface.
+    #  @param self The object pointer.
     def __CreateUI(self):
         # Sizer that all of the buttons will be place into.
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         
         font = wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
+
+        self.__buttonsList = {}
+        self.__defaultButtonDetails = {}
 
         # Array with the order and labels for the buttons.  There are two
         # special buttons: 
@@ -76,6 +85,13 @@ class KeypadPanel(wx.Panel):
             for label in label_list:
                 button = wx.Button(self, label=label)
                 btn_sizer.Add(button, 1, wx.ALIGN_CENTER|wx.EXPAND, 0)
+                self.__buttonsList[button] = button
+
+                self.__defaultButtonDetails[button]  = \
+                {
+                    'backgroundColour' : button.GetBackgroundColour(),
+                    'label' : label
+                }
 
                 if label == 'GO':
                     button.Bind(wx.EVT_BUTTON, self.__TryTransmittingKeyCode)
@@ -91,12 +107,12 @@ class KeypadPanel(wx.Panel):
         self.SetSizer(main_sizer)
 
 
-	## A key is pressed event handler.  If this is the 1st key in the sequence
-	#  then start a timer which is your allotted time to enter the right values
-	#  before they are cleared.  All of the keypresses are stored internally,
-	#  ready for transmission.
-	#  @param self The object pointer.
-	#  @param event Key pressed event object.
+    ## A key is pressed event handler.  If this is the 1st key in the sequence
+    #  then start a timer which is your allotted time to enter the right values
+    #  before they are cleared.  All of the keypresses are stored internally,
+    #  ready for transmission.
+    #  @param self The object pointer.
+    #  @param event Key pressed event object.
     def __PressKey(self, event):
 
         # Get the event object for the key pressed.
@@ -110,15 +126,17 @@ class KeypadPanel(wx.Panel):
         self.__keySequence = self.__keySequence + pressedKeyValue
 
  
+    ## Reset the keypad, which involves clearing key sequence.
+     #  @param self The object pointer.
     def __ResetKeypad(self, event = None):
         self.__keySequence = ''
 
 
-	## Try to transmit the entered key sequence to the alarm master controller.
-	#  The code will only be transmitted if 1 or more keys were pressed, also
-	#  on transmission the sequence timer is stopped and sequence reset.
-	#  @param self The object pointer.
-	#  @param event Unused.
+    ## Try to transmit the entered key sequence to the alarm master controller.
+    #  The code will only be transmitted if 1 or more keys were pressed, also
+    #  on transmission the sequence timer is stopped and sequence reset.
+    #  @param self The object pointer.
+    #  @param event Unused.
     def __TryTransmittingKeyCode(self, event):
         if len(self.__keySequence) == 0:
             return
@@ -167,22 +185,24 @@ class KeypadPanel(wx.Panel):
                     responseText[JsonSchemas.receiveKeyCodeResponse.Actions])
 
 
-	## Timer timeout event function.  This will cause any stored key sequence
-	#  to be cleared and the timer stopped, ready for when the next key is
-	#  pressed.
-	#  @param self The object pointer.
-	#  @param event Unused.
+    ## Timer timeout event function.  This will cause any stored key sequence
+    #  to be cleared and the timer stopped, ready for when the next key is
+    #  pressed.
+    #  @param self The object pointer.
+    #  @param event Unused.
     def __TimeoutEvent(self, event = None):
         self.__ResetKeypad()
         self.__sequenceTimer.Stop()
 
 
-    # Currently we don't do anything with this action except write a debug
-    # message.
+    ## Currently we don't do anything with this action except write a debug
+    #  message.
+    #  @param self The object pointer.
     def __HandleKeycodeRefusedActions(self, actions):
         print('[DEBUG] The keycode was refused!')
 
 
+    #  @param self The object pointer.
     def __HandleKeycodeIncorrectActions(self, actions):
         print('[DEBUG[ The keycode was incorrect')
 
@@ -192,5 +212,34 @@ class KeypadPanel(wx.Panel):
             print('action : disable keypad')
 
 
+    ## STUB
+    #  @param self The object pointer.
     def __HandleKeycodeAcceptedActions(self, actions):
         print('KeycodeAccepted')
+
+
+    ## Disable Keypad wxTimer has timed out event, re-enable the keypad by
+    #  reverting all of the changes made during disabling of it.
+    #  @param self The object pointer.
+    #  @param unused Required parameter for wxTimer but not used.
+    def __keypadDisabledTimedOut(self, unused = None):
+        self.__keypadDisableTimer.Stop()
+
+        for button, defaultValues in self.__defaultButtonDetails.items():
+            button.Enable()
+            button.SetBackgroundColour(defaultValues['backgroundColour'])
+            button.SetLabel(defaultValues['label'])
+
+
+    ## Disable the keypad by setting all the buttons to disabled, changing the
+    #  background to red and removing labels.
+    #  @param self The object pointer.
+     #  @param timeoutSecs Time (seconds) of how long keypad is disabled.
+    def DisableKeypad(self, timeoutSecs):
+        for _, button in self.__buttonsList.items():
+            button.Disable()
+            button.SetBackgroundColour((212, 13, 13))
+            button.SetLabel('')
+
+        self.__keypadDisableTimer.Start(timeoutSecs * 1000)
+
