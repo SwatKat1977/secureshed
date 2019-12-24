f308e8fe22a5d2cfaf4a799827a656b591c22cdc
Paul Morriss <paul.morriss@tokenbay.co.uk>
2019-12-24 21:57:28 +0000

    configuration file changes

--- src/centralController/ConfigurationJsonSchema.py
+++ src/centralController/ConfigurationJsonSchema.py
@@ -105,7 +105,20 @@ CONFIGURATIONJSONSCHEMA = \
                 }
             },
             "required" : ["networkPort"]
+        },
+        "general":
+        {
+            "additionalProperties" : False,
+            "properties":
+            {
+                "additionalProperties" : False,
+                "devicesConfigFile":
+                {
+                    "type" : "string"
+                }
+            },
+            "required" : ["devicesConfigFile"]
         }
     },
-    "required" : ["failedAttemptResponses", "keypadAPI"]
+    "required" : ["failedAttemptResponses", "keypadAPI", "general"]
 }
