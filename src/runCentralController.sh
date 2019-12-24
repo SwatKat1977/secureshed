efcfb53300a1b25e45f40bbfee25426384388ea3
Paul Morriss <paul.morriss@tokenbay.co.uk>
2019-12-24 21:12:28 +0000

    changes to central controller execution script to use public ip address

--- src/runCentralController.sh
+++ src/runCentralController.sh
@@ -4,4 +4,4 @@ export FLASK_ENV=development
 export CENCON_CONFIG=centralController/configuration.json
 export CENCON_DB=centralController/ccontroller.db
 
-python -m flask run -p 2020 --eager-loading --no-reload
+python -m flask run -p 2020 -h 0.0.0.0 --eager-loading --no-reload
