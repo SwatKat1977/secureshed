2f351f624ee8455bad930b1b642975dcec72ea02
Paul Morriss <paul.morriss@tokenbay.co.uk>
2019-12-27 02:34:53 +0000

    added config json schema

--- src/KeypadController/ConfigurationJsonSchema.py
+++ src/KeypadController/ConfigurationJsonSchema.py
@@ -13,3 +13,29 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+
+
+CONFIGURATIONJSONSCHEMA = \
+{
+    "$schema": "http://json-schema.org/draft-07/schema#",
+
+    "type" : "object",
+    "additionalProperties" : False,
+
+    "properties":
+    {
+        "centralController":
+        {
+            "additionalProperties" : False,
+            "properties":
+            {
+                "endpoint":
+                {
+                    "type" : "string"
+                }
+            },
+            "required" : ["endpoint"]
+        }
+    },
+    "required" : ["centralControllerSettings"]
+}
