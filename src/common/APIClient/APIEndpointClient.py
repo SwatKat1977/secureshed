dcf7e81ea16d89fa5d23f758dc418c30deb8d66b
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-22 17:57:32 +0100

    fixes from pylint

--- src/common/APIClient/APIEndpointClient.py
+++ src/common/APIClient/APIEndpointClient.py
@@ -36,24 +36,24 @@ class APIEndpointClient:
         self._lastErrMsg = ''
 
 
-    def SendGetMsg(self, route, mimeType, additionalHeaders = None,
-        body = None):
+    def SendGetMsg(self, route, mimeType, additionalHeaders=None,
+                   body=None):
 
         self._lastErrMsg = ''
         return self.__SendMessage(route, self.__MethodType.Get, mimeType,
-            additionalHeaders, body)
+                                  additionalHeaders, body)
 
 
-    def SendPostMsg(self, route, mimeType, additionalHeaders = None,
-        body = None):
+    def SendPostMsg(self, route, mimeType, additionalHeaders=None,
+                    body=None):
 
         self._lastErrMsg = ''
         return self.__SendMessage(route, self.__MethodType.Post, mimeType,
-            additionalHeaders, body)
+                                  additionalHeaders, body)
 
 
     def __SendMessage(self, route, clientMethodType, mimeType,
-        additionalHeaders, body):
+                      additionalHeaders, body):
 
         url = f'{self.__urlBase}{route}'
 
