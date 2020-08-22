6f2defd486d10a5a3d53aebeac6e64f0d8975bd4
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-22 10:21:38 +0100

    fix scripts for pi

--- src/runKeypadController.sh
+++ src/runKeypadController.sh
@@ -2,4 +2,4 @@ export CENCON_CONFIG=centralController/configuration.json
 export CENCON_DB=centralController/ccontroller.db
 
 cd KeypadController
-python KeypadController.py
+python3 KeypadController.py
