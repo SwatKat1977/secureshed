737a4fb9a064955b53d4ecbeae7f276e94ef6f5b
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-22 19:47:45 +0100

    more changes for directory rename

--- src/CentralController/DeviceManager.py
+++ src/CentralController/DeviceManager.py
@@ -21,7 +21,7 @@ try:
     import RPi.GPIO as GPIO
     RPIO_EMULATED = False
 except ModuleNotFoundError:
-    from centralController.EmulatedRaspberryPiIO import GPIO
+    from CentralController.EmulatedRaspberryPiIO import GPIO
     RPIO_EMULATED = True
 
 
