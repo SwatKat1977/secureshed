4b24ac596393c79d6d3b6af2ccd0e5f703bb643b
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-29 22:35:06 +0100

    added clean script

---
+++ src/clean.sh
@@ -0,0 +1,16 @@
+rm -rf APIs/__pycache__       
+rm -rf APIs/CentralController/__pycache__
+rm -rf APIs/Keypad/__pycache__
+
+rm -rf common/__pycache__
+rm -rf common/APIClient/__pycache__
+
+rm -rf CentralController/__pycache__
+rm -rf CentralController/DeviceTypes/__pycache__
+
+rm -rf KeypadController/__pycache__
+rm -rf KeypadController/Gui/__pycache__
+
+rm -rf PowerConsole/__pycache__
+rm -rf PowerConsole/Gui/__pycache__
+
