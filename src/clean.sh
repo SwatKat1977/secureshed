c52f0da4a2c5e487212078b9e23be5e9326635b4
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-17 23:48:05 +0100

    changes

--- src/clean.sh
+++ src/clean.sh
@@ -5,8 +5,8 @@ rm -rf APIs/Keypad/__pycache__
 rm -rf common/__pycache__
 rm -rf common/APIClient/__pycache__
 
-rm -rf CentralController/__pycache__
-rm -rf CentralController/DeviceTypes/__pycache__
+rm -rf central_controller/__pycache__
+rm -rf central_controller/DeviceTypes/__pycache__
 
 rm -rf KeypadController/__pycache__
 rm -rf KeypadController/Gui/__pycache__
